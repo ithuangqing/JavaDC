65f338e630d13e7e65a3d40f4658700e250fcfd3
ithuangqing <ithuangqing@gmail.com>
2018-09-03 14:50:19 +0800

    提交spring测试南案例

---
+++ SpringMVC案例/src/main/java/com/ithuangqing/controller/Hello.java
@@ -0,0 +1,14 @@
+package com.ithuangqing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Hello {
+    @RequestMapping("/hello")
+    public String test(){
+
+        return "hello";
+
+    }
+}
